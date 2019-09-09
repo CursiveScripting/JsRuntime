a8c9d806400815062456c432b847681b75160a44
FTWinston <ftwinston@gmail.com>
2019-09-09 20:02:37 +0100

    Implemented remaining data type tests

--- src/tests/dataTypes.test.ts
+++ src/tests/dataTypes.test.ts
@@ -3,20 +3,20 @@ import { TypedDataType, FixedType } from '../cursiveruntime/DataType';
 const dtObject = new TypedDataType<object>('object', '#ff0000', false);
 const dtSet = new TypedDataType<Set<object>>('set', '#ff9900', false, dtObject, () => new Set<object>());
 
-const dtString = new FixedType<string>('string', '#cccccc', v => v, null, undefined, new RegExp('.*'));
-const dtFloat = new FixedType<number>('decimal', '#00cc99', v => parseFloat(v), dtString, undefined, new RegExp('^[0-9]+(\.[0-9]*)?$'));
-const dtInt = new FixedType<number>('integer', '#0099ff', v => parseInt(v), dtFloat, undefined, new RegExp('^[0-9]+$'));
+const dtString = new FixedType<string>('string', '#cccccc', v => v, null, () => '', new RegExp('.*'));
+const dtFloat = new FixedType<number>('decimal', '#00cc99', v => parseFloat(v), dtString, () => -1, new RegExp('^[0-9]+(\.[0-9]*)?$'), 'A positive decimal value');
+const dtInt = new FixedType<number>('integer', '#0099ff', v => parseInt(v), dtFloat, () => -1, new RegExp('^[0-9]+$'), 'A positive integer value');
 
 test('Data type is assignable from parent', () => {
-    expect(dtSet.isAssignableTo(dtObject)).toEqual(true);
+    expect(dtSet.isAssignableTo(dtObject)).toBe(true);
 });
 
 test('Data type is not assignable from child', () => {
-    expect(dtObject.isAssignableTo(dtSet)).toEqual(false);
+    expect(dtObject.isAssignableTo(dtSet)).toBe(false);
 });
 
 test('Fixed type is assignable from parent', () => {
-    expect(dtInt.isAssignableTo(dtFloat)).toEqual(true);
+    expect(dtInt.isAssignableTo(dtFloat)).toBe(true);
 });
 
 test('Fixed type is not assignable from child', () => {
@@ -24,13 +24,62 @@ test('Fixed type is not assignable from child', () => {
 });
 
 test('Fixed type is assignable from grandparent', () => {
-    expect(dtInt.isAssignableTo(dtString)).toEqual(true);
+    expect(dtInt.isAssignableTo(dtString)).toBe(true);
 });
 
 test('Fixed type is not assignable from grandchild', () => {
-    expect(dtString.isAssignableTo(dtInt)).toEqual(false);
+    expect(dtString.isAssignableTo(dtInt)).toBe(false);
 });
 
 test('Data type is not assignable from unrelated type', () => {
-    expect(dtString.isAssignableTo(dtSet)).toEqual(false);
+    expect(dtString.isAssignableTo(dtSet)).toBe(false);
+});
+
+test('Unspecified reference type default is null', () => {
+    expect(dtObject.getDefaultValue()).toBeNull();
+});
+
+test('Reference type default is not null', () => {
+    expect(dtSet.getDefaultValue()).not.toBeNull();
+});
+
+test('Reference type default is correct type', () => {
+    const defaultVal = dtSet.getDefaultValue();
+    expect(defaultVal instanceof Set).toBe(true);
+});
+
+test('Reference type default instances differ', () => {
+    const default1 = dtSet.getDefaultValue();
+    const default2 = dtSet.getDefaultValue();
+    expect(default1).not.toBe(default2);
+});
+
+test('Value type default is not null', () => {
+    expect(dtInt.getDefaultValue()).not.toBeNull();
+});
+
+test('Value type uses specified default', () => {
+    expect(dtInt.getDefaultValue()).toBe(-1);
+});
+
+test('Valid value validates', () => {
+    expect(dtInt.validation).not.toBeUndefined();
+    expect(dtInt.validation!.test('2'));
+});
+
+test('Invalid value doesn\'t validate', () => {
+    expect(dtInt.validation).not.toBeUndefined();
+    expect(dtInt.validation!.test('-2'));
+});
+
+test('Parses valid value', () => {
+    expect(dtInt.deserialize('2')).toBe(2);
+});
+
+test('Guidance is present', () => {
+    expect(dtInt.guidance).not.toBeUndefined();
+});
+
+test('Unset guidance is undefined', () => {
+    expect(dtObject.guidance).toBeUndefined(); 
 });
--- src/tests/systemProcesses.test.ts
+++ src/tests/systemProcesses.test.ts
@@ -5,7 +5,7 @@ test('Compare function returns less', async () => {
 
     const result = await workspace.compare(1, 2);
 
-    expect(result).toEqual('less');
+    expect(result).toBe('less');
 });
 
 test('Compare function returns greater', async () => {
@@ -13,7 +13,7 @@ test('Compare function returns greater', async () => {
 
     const result = await workspace.compare(5, 1);
 
-    expect(result).toEqual('greater');
+    expect(result).toBe('greater');
 });
 
 test('Compare function returns equal', async () => {
@@ -21,7 +21,7 @@ test('Compare function returns equal', async () => {
 
     const result = await workspace.compare(7, 7);
 
-    expect(result).toEqual('equal');
+    expect(result).toBe('equal');
 });
 
 test('Adds 1 + 2 to get 3', async () => {
@@ -29,7 +29,7 @@ test('Adds 1 + 2 to get 3', async () => {
 
     const result = await workspace.add(1, 2);
 
-    expect(result).toEqual(3);
+    expect(result).toBe(3);
 });
 
 test('Adds 0 + 0 to get 0', async () => {
@@ -37,7 +37,7 @@ test('Adds 0 + 0 to get 0', async () => {
 
     const result = await workspace.add(0, 0);
 
-    expect(result).toEqual(0);
+    expect(result).toBe(0);
 });
 
 test('Adds 13 + 7 to get 20', async () => {
@@ -45,5 +45,5 @@ test('Adds 13 + 7 to get 20', async () => {
 
     const result = await workspace.add(13, 7);
 
-    expect(result).toEqual(20);
+    expect(result).toBe(20);
 });
