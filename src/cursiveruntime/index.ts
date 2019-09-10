d1d6083ee3bc285e1acaeee1091bb367cd32ee06
FTWinston <ftwinston@gmail.com>
2019-09-10 18:13:21 +0100

    Preparing package

---
+++ src/cursiveruntime/index.ts
@@ -0,0 +1,9 @@
+export { DataType } from './DataType';
+export { FixedType } from './FixedType';
+export { TypedDataType } from './TypedDataType';
+export { LookupType } from './LookupType';
+export { RequiredProcess } from './RequiredProcess';
+export { SystemProcess } from './SystemProcess';
+export { ValueSet } from './ValueSet';
+export { Workspace } from './Workspace';
+//export { IUserProcessData } from './services/serializedDataModels';
